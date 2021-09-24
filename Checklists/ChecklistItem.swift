a3a176b16b2c7941bb7fd2860395e601b4f91c51
Michelle Guthrie-Harrigan <mguthr5@live.spcollege.edu>
2021-09-23 20:12:54 -0400

    Completed Checklist

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Michelle Guthrie-Harrigan on 9/22/21.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = false
+}
